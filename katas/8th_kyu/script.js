0712e6ab69b37ccacc16b3cbabef79bfbd1c64ac
Kalliran <theautumn90@gmail.com>
2023-12-11 12:52:11 -0600

    added daily codewars challenge to script.js

--- katas/8th_kyu/script.js
+++ katas/8th_kyu/script.js
@@ -6,3 +6,10 @@ const grow=x=> x.reduce((a,b) => a*b);
 // function grow(x){
 //     return x.reduce((acc, c) => acc*c);
 //   }
+
+//You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.Array can contain numbers or strings. X can be either.Return true if the array contains the value, false if not.
+
+function check(a, x) {
+    // your code here
+    
+  }
