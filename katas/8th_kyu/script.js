8ff2ec136553d98441f0289fc20f6f57d66e644e
Kalliran <theautumn90@gmail.com>
2023-12-13 10:56:25 -0600

    adding sum of positive exercise

--- katas/8th_kyu/script.js
+++ katas/8th_kyu/script.js
@@ -6,10 +6,27 @@ const grow=x=> x.reduce((a,b) => a*b);
 // function grow(x){
 //     return x.reduce((acc, c) => acc*c);
 //   }
-
-//You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.Array can contain numbers or strings. X can be either.Return true if the array contains the value, false if not.
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//You will be given an array a and a value x. All you need to do is check whether the provided array contains the value. Array can contain numbers or strings. X can be either.Return true if the array contains the value, false if not.
 
 function check(a, x) {
     // your code here
-    
+    return a.includes(x)
+}
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//You get an array of numbers, return the sum of all of the positives ones.
+
+// Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+// Note: if there is nothing to sum, the sum is default to 0.
+
+function positiveSum(arr) {
+  let total = 0;    
+  for (i = 0; i < arr.length; i++) {    // setup loop to go through array of given length
+    if (arr[i] > 0) {                   // if arr[i] is greater than zero, ensures removal of negative elements
+      total += arr[i];                  // add arr[i] to total
+    }
   }
+  return total;                         // return total
+}
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
