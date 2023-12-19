2bfe2c9accb6efa39002cf08074185aea33672f0
Kalliran <theautumn90@gmail.com>
2023-12-19 12:49:33 -0600

    completed a kata in 8th kyu

--- katas/8th_kyu/script.js
+++ katas/8th_kyu/script.js
@@ -37,3 +37,17 @@ function positiveSum(arr) {
 function greet(name){
   return `Hello, ${name} how are you doing today?`;
 }
+//-----------------------------------------------------------------
+// Write a function that will check if two given characters are the same case.
+// If either of the characters is not a letter, return -1
+// If both characters are the same case, return 1
+// if both characters are letters, but not the same case, return 0
+function sameCase(a, b){
+  if(a.toUpperCase() === a.toLowerCase() || b.toLowerCase() === b.toUpperCase()){
+    return -1
+    }else if(a === a.toLowerCase() && b === b.toLowerCase() || a === a.toUpperCase() && b === b.toUpperCase()){
+        return 1
+    }else{
+    return 0
+    }
+}
