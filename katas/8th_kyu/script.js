d1ba5d175780255b5a349e7407a00455ef553744
Kalliran <theautumn90@gmail.com>
2023-12-22 11:47:21 -0600

    completed an 8th kyu kata

--- katas/8th_kyu/script.js
+++ katas/8th_kyu/script.js
@@ -59,3 +59,33 @@ function sameCase(a, b){
 function noSpace(x){
   return x.replaceAll(' ', '');
 }
+//------------------------------------------------------------------
+// Your task is to create a function that does four basic mathematical operations.
+// The function should take three arguments - operation(string/char), value1(number), value2(number).
+// The function should return result of numbers after applying the chosen operation.
+// Examples(Operator, value1, value2) --> output
+// ('+', 4, 7) --> 11
+// ('-', 15, 18) --> -3
+// ('*', 5, 5) --> 25
+// ('/', 49, 7) --> 7
+function basicOp(operation, value1, value2){
+  if (operation == '+'){
+    return value1 + value2;
+  }else if(operation == '-'){
+    return value1 - value2;
+  }else if(operation == '*'){
+    return value1 * value2;
+  }else if(operation == '/'){
+    return value1 / value2;
+  }else{
+    return 0;
+  }
+}
+// -same thing, just ternary
+function basic(operation, val1, val2){
+  return operation == '+' ? val1 + val2 :
+   operation == '-' ? val1 - val2 :
+    operation == '*' ? val1 * val2 :
+     operation == '/' ? val1 / val2 :
+      0;
+}
