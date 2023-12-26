e7b8f6637bc1acc71d6254a13afae4c1cba5fa53
Kalliran <theautumn90@gmail.com>
2023-12-26 14:43:04 -0600

    completed a 7th kyu kata

--- katas/7th_kyu/script.js
+++ katas/7th_kyu/script.js
@@ -55,3 +55,9 @@ function getMiddle(s){
         return s[Math.floor(s.length / 2)]  //return one middle char
     }
 }
+//-------------------------------------------------------------------------------------------
+//In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+function filter_list(l){
+    // Return a new array with the strings filtered out
+    return l.filter((e) => Number.isInteger(e));
+}
