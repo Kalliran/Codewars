30c8728d435203aa398f33c39d9070368a5cccd2
Kalliran <theautumn90@gmail.com>
2023-12-27 09:18:09 -0600

    completed a 7th kyu kata and added html file for testing

--- katas/7th_kyu/script.js
+++ katas/7th_kyu/script.js
@@ -18,7 +18,7 @@ function getCount(str){
             count++;
         }
     });
-    console.log(count)
+    // console.log(count)
 }
 
 getCount("abracadabra");
@@ -60,4 +60,10 @@ function getMiddle(s){
 function filter_list(l){
     // Return a new array with the strings filtered out
     return l.filter((e) => Number.isInteger(e));
+}
+//-------------------------------------------------------------------------------------------
+//This time no story, no theory. The examples below show you how to write function accum:
+//The parameter of accum is a string which includes only letters from a..z and A..Z.
+function accum(s){
+    return s.split('').map((c,i) => (c.toUpperCase() + c.toLowerCase().repeat(i))).join('-');
 }
