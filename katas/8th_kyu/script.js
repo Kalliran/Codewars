68f1fc3538fdb70a8dc4b29bd8bba1177b54fb78
Kalliran <theautumn90@gmail.com>
2024-01-05 08:27:40 -0600

    completed an 8th kyu kata

--- katas/8th_kyu/script.js
+++ katas/8th_kyu/script.js
@@ -100,3 +100,19 @@ class SmallestIntegerFinder {
     return Math.min(...args)
   }
 }
+//------------------------------------------------------------------
+// You want to know if you're better than the average student in your class.
+// You receive an array with your peers' test scores. Now calculate the average and compare your score!
+// Return True if you're better, else False!
+// Your points are not included in the array of your class's points. For calculating the average point you may add your point to the given array!
+function betterThanAverage(classPoints, yourPoints){
+  classPoints = []
+  yourPoints = []
+  let classAvg = classPoints.reduce((acc, c) => acc + c, 0) / classPoints.length
+  let yourAvg = yourPoints.reduce((acc, c) => acc + c, 0) / yourPoints.length
+  if(yourAvg >= classAvg){
+    return true
+  }else{
+    return false
+  }
+}
