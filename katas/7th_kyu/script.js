a234d39095084e9259a50f4c51f164b1857a1789
Kalliran <theautumn90@gmail.com>
2024-01-06 09:06:39 -0600

    commiting 7th kyu kata

--- katas/7th_kyu/script.js
+++ katas/7th_kyu/script.js
@@ -148,9 +148,36 @@ const addBinary = (a, b) => (a+b).toString(2) //decimal to binary with the Numbe
 // Make a program that filters a list of strings and returns a list with only your friends name in it.
 // If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
 // Ex: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"]
-// i.e.
 // friend ["Ryan", "Kieran", "Mark"] `shouldBe` ["Ryan", "Mark"]
 // Note: keep the original order of the names in the output.
 const friend = (friends) => {
     return friends.filter(str => str.length === 4)
+}
+//-------------------------------------------------------------------------------------------
+// Build a pyramid-shaped tower, as an array/list of strings, given a positive integer number of floors. A tower block is represented with "*" character.
+// For example, a tower with 3 floors looks like this:
+// [
+//   "  *  ",
+//   " *** ", 
+//   "*****"
+// ]
+// And a tower with 6 floors looks like this:
+// [
+//   "     *     ", 
+//   "    ***    ", 
+//   "   *****   ", 
+//   "  *******  ", 
+//   " ********* ", 
+//   "***********"
+// ]
+function towerBuilder(nFloors){
+    let space = "";
+    let star = "";
+    let result = [];
+    for(let i = 1; i <= nFloors; i++){
+        space = (" ").repeat(nFloors - i);
+        star = ("*").repeat((2 * i) - i);
+        result.push(space + star + space);
+    }
+    return result;
 }
