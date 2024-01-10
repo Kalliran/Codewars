3aab9822e3b92ea4efb34301255d27566627d474
Kalliran <theautumn90@gmail.com>
2024-01-10 08:02:24 -0600

    completed 8th kyu kata

--- katas/8th_kyu/script.js
+++ katas/8th_kyu/script.js
@@ -108,3 +108,15 @@ class SmallestIntegerFinder {
 function betterThanAverage(classPoints, yourPoints){
   return yourPoints > classPoints.reduce((acc, c) => acc + c, 0) / classPoints.length
 }
+//------------------------------------------------------------------
+// Can you find the needle in the haystack?
+// Write a function findNeedle() that takes an array full of junk but containing one "needle"
+// After your function finds the needle it should return a message (as a string) that says:
+// "found the needle at position " plus the index it found the needle, so:
+// Example(Input --> Output)
+// ["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"] --> "found the needle at position 5" 
+function findNeedle(haystack){
+  if(haystack.includes('needle')){
+   return `found the needle at position ${haystack.indexOf('needle')}`
+  }
+}
