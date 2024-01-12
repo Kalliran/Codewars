9560bc2509caa84fd0c4d2525713b297cc12be44
Kalliran <theautumn90@gmail.com>
2024-01-12 08:29:39 -0600

    completed 7th kyu kata

--- katas/7th_kyu/script.js
+++ katas/7th_kyu/script.js
@@ -216,4 +216,18 @@ function createBox(m, n) {
       }
     }
     return result
-  }
+}
+//----------------------------------------------------------------------
+// Complete the findNextSquare method that finds the next integral perfect square after the one passed as a parameter. Recall that an integral perfect square is an integer n such that sqrt(n) is also an integer. The square root of a number is an integer.
+// If the parameter is itself not a perfect square then -1 should be returned. You may assume the parameter is non-negative.
+// Examples:(Input --> Output)
+// 121 --> 144
+// 625 --> 676
+// 114 --> -1 since 114 is not a perfect square
+function findNextSquare(sq) {
+    // Return the next square if sq is a perfect square, -1 otherwise
+    if(Math.sqrt(sq) % 1){
+        return -1;
+    }
+    return (Math.sqrt(sq) + 1) ** 2
+}
