386cc8755a2b3d4bb086c8919e49d889c0794458
Kalliran <theautumn90@gmail.com>
2024-01-17 10:31:27 -0600

    completed 8th kyu kata

--- katas/8th_kyu/script.js
+++ katas/8th_kyu/script.js
@@ -200,3 +200,10 @@ function paperwork(n, m) {
   }
   return n * m;
 }
+//------------------------------------------------------------------
+// Take an array and remove every second element from the array. Always keep the first element and start removing with the next element.
+// ["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]
+// None of the arrays will be empty, so you don't have to worry about that!
+function removeEveryOther(arr){
+  return arr.filter((_, index) => index % 2 === 0) //if the element's index is even, it is filtered out
+}
