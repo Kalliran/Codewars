3ccef0d6b71c57edfa773e4a63202f1629b4c70f
Kalliran <theautumn90@gmail.com>
2024-01-17 09:56:34 -0600

    completed 8th kyu kata

--- katas/8th_kyu/script.js
+++ katas/8th_kyu/script.js
@@ -189,3 +189,14 @@ String.prototype.toAlternatingCase = function () {
 // Example
 // ['hello', 'world', 'this', 'is', 'great']  =>  'hello world this is great'
 const smash = words => words.join(' ')
+//------------------------------------------------------------------
+// Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages.
+// Your task is to calculate how many blank pages do you need. If n < 0 or m < 0 return 0.
+// n= 5, m=5: 25
+// n=-5, m=5:  0
+function paperwork(n, m) {
+  if(n < 0 || m < 0){
+    return 0
+  }
+  return n * m;
+}
