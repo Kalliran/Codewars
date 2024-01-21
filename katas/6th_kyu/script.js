9c0907e011947a75853274cf158637477b13c393
unknown <theautumn90@gmail.com>
2024-01-21 10:21:13 -0600

    completed a 6th kyu kata

--- katas/6th_kyu/script.js
+++ katas/6th_kyu/script.js
@@ -19,7 +19,7 @@ function findOdd(A) {
     }
   }
 }
-//---------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------
 // Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
 // It should remove all values from list a, which are present in list b keeping their order.
 // Examples
@@ -36,4 +36,49 @@ const arrayDiff = (a, b) => {
   //   return a;
   a = a.filter(val => !b.includes(val))
   return a;
+}
+//-----------------------------------------------------------------------
+// Given an array (arr) as an argument complete the function countSmileys that should return the total number of smiling faces.
+// Rules for a smiling face:
+// Each smiley face must contain a valid pair of eyes. Eyes can be marked as : or ;
+// A smiley face can have a nose but it does not have to. Valid characters for a nose are - or ~
+// Every smiling face must have a smiling mouth that should be marked with either ) or D
+// No additional characters are allowed except for those mentioned.
+// Valid smiley face examples: :) :D ;-D :~)
+// Invalid smiley faces: ;( :> :} :]
+// Example
+// countSmileys([':)', ';(', ';}', ':-D']);       // should return 2;
+// countSmileys([';D', ':-(', ':-)', ';~)']);     // should return 3;
+// countSmileys([';]', ':[', ';*', ':$', ';-D']); // should return 1;
+// Note
+// In case of an empty array return 0. You will not be tested with invalid input (input will always be an array). Order of the face (eyes, nose, mouth) elements will always be the same.
+//return the total number of smiling faces in the array
+// : or ;
+// - or ~
+// ) or D
+const countSmileys = (arr) => {
+  if( arr.length === 0) {
+    return 0;
+  }
+  let i = 0;
+  let validSmileys = [
+    ":)",
+    ";)",
+    ":D",
+    ";D",
+    ":-D",
+    ":~D",
+    ":-)",
+    ":~)",
+    ";~D",
+    ";~)",
+    ";-D",
+    ";-)"
+  ]
+  for (let key of arr) {
+    if (validSmileys.includes(key)) {
+      i++;
+    }
+  }
+  return i;
 }
