4d811ed7ed093da5b78da057f2baae5e22c803a8
Kalliran <theautumn90@gmail.com>
2024-01-23 08:49:50 -0600

    completed 8th kyu kata

--- katas/8th_kyu/script.js
+++ katas/8th_kyu/script.js
@@ -292,3 +292,18 @@ function getGrade (s1, s2, s3) {
     return 'A'
   }
 }
+//------------------------------------------------------------------
+// Nathan loves cycling.
+// Because Nathan knows it is important to stay hydrated, he drinks 0.5 litres of water per hour of cycling.
+// You get given the time in hours and you need to return the number of litres Nathan will drink, rounded to the smallest value.
+// For example:
+// time = 3 ----> litres = 1
+// time = 6.7---> litres = 3
+// time = 11.8--> litres = 5
+function litres(time) {
+  //0.5 litres per hour
+  //time in hours, round down
+  let liters = 0.5;
+  let lph = Math.floor(liters * time)
+  return lph;
+}
