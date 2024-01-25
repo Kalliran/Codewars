4d0ff8e16c085a28d79d597d4273b59eb193aaae
Kalliran <theautumn90@gmail.com>
2024-01-25 07:38:22 -0600

    completed 8th kyu kata

--- katas/8th_kyu/script.js
+++ katas/8th_kyu/script.js
@@ -315,3 +315,20 @@ function litres(time) {
 function twiceAsOld(dadYearsOld, sonYearsOld) {
   return Math.abs(dadYearsOld - 2 * sonYearsOld)
 }
+//------------------------------------------------------------------
+// Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+// Use conditionals to return the proper message:
+// case	return
+// name equals owner	'Hello boss'
+// otherwise	'Hello guest'
+
+// function greet (name, owner) {
+//   if(name == owner){
+//     return 'Hello boss'
+//   }else{
+//     return 'Hello guest'
+//   }
+// }
+
+// same but with arrow function and ternary if statement
+const greet = (name, owner) => {return name == owner ? 'Hello boss' : 'Hello guest';}
