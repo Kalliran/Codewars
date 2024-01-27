e7d95dbe097abaeea12b6cd3654401ce22bda31d
unknown <theautumn90@gmail.com>
2024-01-27 08:13:23 -0600

    completed 6th kyu kata

--- katas/6th_kyu/script.js
+++ katas/6th_kyu/script.js
@@ -82,3 +82,7 @@ const countSmileys = (arr) => {
   }
   return i;
 }
+//------------------------------------------------------------------
+// The main idea is to count all the occurring characters in a string. If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
+// What if the string is empty? Then the result should be empty object literal, {}.
+const count = (string) => require('lodash').countBy(string);
