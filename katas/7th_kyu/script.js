a92b1b6a9685c2510de303fa90791d2f696577ac
Kalliran <theautumn90@gmail.com>
2024-01-31 12:51:45 -0600

    completed 7th kyu kata

--- katas/7th_kyu/script.js
+++ katas/7th_kyu/script.js
@@ -239,3 +239,11 @@ function findNextSquare(sq) {
 // [] --> []
 // ["a", "b", "c"] --> ["1: a", "2: b", "3: c"]
 const number = array => array.map((element, index) => `${index + 1}: ${element}`)
+//----------------------------------------------------------------------
+// Create a function that returns the sum of the two lowest positive numbers given an array of minimum 4 positive integers. No floats or non-positive integers will be passed.
+// For example, when an array is passed like [19, 5, 42, 2, 77], the output should be 7.
+// [10, 343445353, 3453445, 3453545353453] should return 3453455.
+function sumTwoSmallestNumbers(numbers) {  
+    let smallest = numbers.sort((a,b) => a - b).slice(0, 2).reduce();
+    return smallest;
+}
