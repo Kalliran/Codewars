b8c5a1236de161983fa77808943ca635aa1df8f0
Kalliran <theautumn90@gmail.com>
2024-02-14 09:14:23 -0600

    completed 8th kyu kata

--- katas/8th_kyu/script.js
+++ katas/8th_kyu/script.js
@@ -397,3 +397,14 @@ const feast = (beast, dish) => {
     return false
   }
 }
+//------------------------------------------------------------------
+// In this kata you need to check the provided array (x) for good ideas 'good' and bad ideas 'bad'. If there are one or two good ideas, return 'Publish!', if there are more than 2 return 'I smell a series!'. If there are no good ideas, as is often the case, return 'Fail!'.
+function well (x) {
+  let goodIdeaCount = 0;
+  for (let idea of x) {
+    if(idea === 'good') goodIdeaCount += 1;
+  }
+  if(goodIdeaCount > 2) return 'I smell a series!';
+  if(goodIdeaCount > 0) return 'Publish!';
+  return 'Fail!';
+}
