452b0362fb20dbc21625d72567a6a575d682c160
Kalliran <theautumn90@gmail.com>
2024-02-17 08:18:06 -0600

    completed 8th kyu kata

--- katas/8th_kyu/script.js
+++ katas/8th_kyu/script.js
@@ -431,3 +431,11 @@ function areYouPlayingBanjo(name) {
 function fakeBin(x){
  return x.split('').map(x => x < 5 ? 0 : 1).join('');
 }
+//------------------------------------------------------------------
+// Write a function to split a string and convert it into an array of words.
+// Examples (Input ==> Output):
+// "Robin Singh" ==> ["Robin", "Singh"]
+// "I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+function stringToArray(string){
+  return string.split(' ');
+}
