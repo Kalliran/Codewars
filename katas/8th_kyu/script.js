bf7d14d5dd8a4e2ff64b851161bcc71a79990ab5
Kalliran <theautumn90@gmail.com>
2024-02-18 07:25:16 -0600

    completed 8th kyu kata

--- katas/8th_kyu/script.js
+++ katas/8th_kyu/script.js
@@ -439,3 +439,14 @@ function fakeBin(x){
 function stringToArray(string){
   return string.split(' ');
 }
+//------------------------------------------------------------------
+// If you can't sleep, just count sheep!!
+// Task:
+// Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
+var countSheep = function (num){
+  let sheepString = '';
+  for(let i = 1; i <= num; i++){
+    sheepString += i.toString() + ' sheep...'
+  }
+  return sheepString;
+}
