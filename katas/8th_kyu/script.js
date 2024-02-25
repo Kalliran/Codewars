070023cf6b2c83890f5ce44015ba26fd31cecd40
Kalliran <theautumn90@gmail.com>
2024-02-25 09:49:36 -0600

    completed 8th kyu kata

--- katas/8th_kyu/script.js
+++ katas/8th_kyu/script.js
@@ -429,3 +429,33 @@ function bonusTime(salary, bonus) {
     return "£" + salary;
   }
 }
+//------------------------------------------------------------------
+// Character recognition software is widely used to digitise printed texts. Thus the texts can be edited, searched and stored on a computer.
+
+// When documents (especially pretty old ones written with a typewriter), are digitised character recognition softwares often make mistakes.
+
+// Your task is correct the errors in the digitised text. You only have to handle the following mistakes:
+
+// S is misinterpreted as 5
+// O is misinterpreted as 0
+// I is misinterpreted as 1
+// The test cases contain numbers only by mistake.
+function correct(string) {
+  let correctedText = "";
+  for(let i = 0; i < string.length; i++){
+    switch(string[i]){
+      case '5':
+        correctedText += 'S';
+        break;
+      case '0':
+        correctedText += 'O';
+        break;
+      case '1':
+        correctedText += 'I';
+        break;
+      default:
+        correctedText += string[i];
+    }
+  }
+  return correctedText;
+}
