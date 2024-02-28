a0a406e5bcdad7e649c355b4bf68948e66c68504
Kalliran <theautumn90@gmail.com>
2024-02-28 08:57:30 -0600

    completed 8th kyu kata

--- katas/8th_kyu/script.js
+++ katas/8th_kyu/script.js
@@ -480,3 +480,20 @@ function shortcut(string){
 
 // You should not remove or add elements from/to the array.
 const twoSort = (s) => s.sort()[0].split('').join('***');
+//------------------------------------------------------------------
+// Given 2 strings, a and b, return a string of the form short+long+short, with the shorter string on the outside and the longer string on the inside. The strings will not be the same length, but they may be empty ( zero length ).
+
+// Hint for R users:
+
+// The length of string is not always the same as the number of characters
+// For example: (Input1, Input2) --> output
+
+// ("1", "22") --> "1221"
+// ("22", "1") --> "1221"
+const solution = (a, b) => {
+  if(a.length > b.length){
+    return b + a + b;
+  }else{
+    return a + b + a;
+  }
+}
