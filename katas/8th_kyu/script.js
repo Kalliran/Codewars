f4030463adc08d949a5c959e9b3d84836c5384f8
Kalliran <theautumn90@gmail.com>
2024-03-01 08:00:22 -0600

    completed 8th kyu kata

--- katas/8th_kyu/script.js
+++ katas/8th_kyu/script.js
@@ -497,3 +497,24 @@ const solution = (a, b) => {
     return a + b + a;
   }
 }
+//------------------------------------------------------------------
+// The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
+
+// Task
+// Given a year, return the century it is in.
+
+// Examples
+// 1705 --> 18
+// 1900 --> 19
+// 1601 --> 17
+// 2000 --> 20
+// 2742 --> 28
+function century (year) {
+  // We need 'year' to be a number otherwise it remains a string. Putting a '+' plus sign in front of the variable 'year' keeps it as an integer.
+  if(year.toString().length < 3) return 1;
+  const firstTwoDigits = +year.toString().slice(0, -2);
+  if (year % 100 === 0) return firstTwoDigits;
+  return firstTwoDigits + 1;
+}
+// Another solution:
+const centuryRefactored = year => Math.ceil(year/100);
