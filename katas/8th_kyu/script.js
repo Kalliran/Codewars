567b8c7400545f1f8dbb08a0fa4e798725f78c13
Kalliran <theautumn90@gmail.com>
2024-03-02 09:25:56 -0600

    completed 8th kyu kata

--- katas/8th_kyu/script.js
+++ katas/8th_kyu/script.js
@@ -518,3 +518,8 @@ function century (year) {
 }
 // Another solution:
 const centuryRefactored = year => Math.ceil(year/100)
+//------------------------------------------------------------------
+// Write a function that returns a string in which firstname is swapped with last name.
+// Example(Input --> Output)
+// "john McClane" --> "McClane john"
+const nameShuffler = str => str.split(' ').reverse().join(' ');
