4fd572a8ea5fc66d15d8e392f7d9ede097dcf07c
Kalliran <theautumn90@gmail.com>
2024-03-04 10:40:05 -0600

    completed 8th kyu katas

--- katas/8th_kyu/script.js
+++ katas/8th_kyu/script.js
@@ -540,3 +540,26 @@ const nameShuffler = str => str.split(' ').reverse().join(' ');
 // 0 <= m <= 59
 // 0 <= s <= 59
 const past = (h, m, s) => (h * 60 * 60 * 1000) + (m * 60 * 1000) + (s * 1000);
+//------------------------------------------------------------------
+// Write a function that takes an array of numbers and returns the sum of the numbers. The numbers can be negative or non-integer. If the array does not contain any numbers then you should return 0.
+
+// Examples
+// Input: [1, 5.2, 4, 0, -1]
+// Output: 9.2
+
+// Input: []
+// Output: 0
+
+// Input: [-2.398]
+// Output: -2.398
+
+// Assumptions
+// You can assume that you are only given numbers.
+// You cannot assume the size of the array.
+// You can assume that you do get an array and if the array is empty, return 0.
+// What We're Testing
+// We're testing basic loops and math operations. This is for beginners who are just learning loops and math operations.
+const sum = numbers => numbers.reduce((acc, c) => acc + c, 0);
+//------------------------------------------------------------------
+// Now you have to write a function that takes an argument and returns the square of it.
+const square = num => Math.pow(num, 2);
