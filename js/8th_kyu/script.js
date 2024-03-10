83291f0c32eabef65f15c6d50f29d351b200727a
Kalliran <theautumn90@gmail.com>
2024-03-10 08:12:03 -0500

    completed 8th ky kata

--- js/8th_kyu/script.js
+++ js/8th_kyu/script.js
@@ -634,3 +634,19 @@ function peopleWithAgeDrink(old){
     return 'drink whisky';
   }
 }
+//------------------------------------------------------------------
+// Create a function with two arguments that will return an array of the first n multiples of x.
+
+// Assume both the given number and the number of times to count will be positive numbers greater than 0.
+
+// Return the results as an array or list ( depending on language ).
+
+// Examples
+// countBy(1,10) === [1,2,3,4,5,6,7,8,9,10]
+// countBy(2,5) === [2,4,6,8,10]
+function arrayPlusArray(arr1, arr2) {
+  const combined = [...arr1, ...arr2];
+  return combined.reduce((total, n) => {
+    return total + n
+  }, 0);
+}
