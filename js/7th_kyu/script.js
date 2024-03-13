091e09a033722a949dd5db828617504ea514d6f3
Kalliran <theautumn90@gmail.com>
2024-03-13 09:28:58 -0500

    completed 7th kyu kata

--- js/7th_kyu/script.js
+++ js/7th_kyu/script.js
@@ -319,3 +319,19 @@ const solution = (str, ending) => str.endsWith(ending);
 
 // Input: 123456789 Output: 987654321
 const descendingOrder = n => Number([...String(n)].sort((a, b) => b - a).join(''));
+//------------------------------------------------------------------
+// Implement a function that accepts 3 integer values a, b, c. The function should return true if a triangle can be built with the sides of given length and false in any other case.
+
+// (In this case, all triangles must have surface greater than 0 to be accepted).
+
+// Examples:
+
+// Input -> Output
+// 1,2,2 -> true
+// 4,2,3 -> true
+// 2,2,2 -> true
+// 1,2,3 -> false
+// -5,1,3 -> false
+// 0,2,3 -> false
+// 1,2,9 -> false 
+const isTriangle = (a,b,c) => a + b > c && b + c > a && c + a > b;
