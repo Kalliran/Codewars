e0e72b5e86c26000f8fdcfef0c1a9f1aecb4504f
Kalliran <theautumn90@gmail.com>
2024-03-13 09:17:26 -0500

    completed 8th kyu kata

--- js/8th_kyu/script.js
+++ js/8th_kyu/script.js
@@ -680,3 +680,16 @@ const quarterOf = (month) => {
 }
 //another solution
 const quartersOf = month => Math.ceil(month/3);
+//------------------------------------------------------------------
+// Every day you rent the car costs $40. If you rent the car for 7 or more days, you get $50 off your total. Alternatively, if you rent the car for 3 or more days, you get $20 off your total.
+// Write a code that gives out the total amount for different days(d).
+function rentalCarCost (d) {
+  let total = d * 40;
+  if(d >= 7 && d > 3){
+    return total -= 50;
+  } else if (d >= 3 && d < 7){
+    return total -= 20;
+  } else {
+    return total;
+  }
+}
