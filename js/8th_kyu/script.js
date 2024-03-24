e9469dfcb141797ec9a49d1be4d471145cdeceab
Kalliran <theautumn90@gmail.com>
2024-03-24 07:43:47 -0500

    completed 8th kyu kata

--- js/8th_kyu/script.js
+++ js/8th_kyu/script.js
@@ -807,3 +807,20 @@ function switchItUp(number) {
       return 'Enter a number 1 - 9';
   }
 }
+//------------------------------------------------------------------
+// Messi is a soccer player with goals in three leagues:
+
+// LaLiga
+// Copa del Rey
+// Champions
+// Complete the function to return his total number of goals in all three leagues.
+
+// Note: the input will always be valid.
+
+// For example:
+
+// 5, 10, 2  -->  17
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  // code goes here
+  return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+}
