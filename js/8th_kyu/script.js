70ed1bad8c73b3cbf8d70d245574a27a32329799
Kalliran <theautumn90@gmail.com>
2024-03-25 08:33:23 -0500

    completed 8th kyu kata

--- js/8th_kyu/script.js
+++ js/8th_kyu/script.js
@@ -824,3 +824,19 @@ function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
   // code goes here
   return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
 }
+//------------------------------------------------------------------
+
+// Complete the function that takes two integers (a, b, where a < b) and return an array of all integers between the input parameters, including them.
+
+// For example:
+
+// a = 1
+// b = 4
+// --> [1, 2, 3, 4]
+function between(a, b) {
+  const arr = [];
+  for (let i = a; i <= b; i++){
+    arr.push(i);
+  }
+  return arr;
+}
