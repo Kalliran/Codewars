7b93c44e00d59b2f97397534691c071f9e3850b7
Kalliran <theautumn90@gmail.com>
2024-03-26 07:42:03 -0500

    completed 8th kyu kata

--- js/8th_kyu/script.js
+++ js/8th_kyu/script.js
@@ -825,7 +825,6 @@ function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
   return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
 }
 //------------------------------------------------------------------
-
 // Complete the function that takes two integers (a, b, where a < b) and return an array of all integers between the input parameters, including them.
 
 // For example:
@@ -839,4 +838,28 @@ function between(a, b) {
     arr.push(i);
   }
   return arr;
+}
+//------------------------------------------------------------------
+// You are given a string containing a sequence of character sequences separated by commas.
+
+// Write a function which returns a new string containing the same character sequences except the first and the last ones but this time separated by spaces.
+
+// If the input string is empty or the removal of the first and last items would cause the resulting string to be empty, return an empty value (represented as a generic value NULL in the examples below).
+
+// Examples
+// "1,2,3"      =>  "2"
+// "1,2,3,4"    =>  "2 3"
+// "1,2,3,4,5"  =>  "2 3 4"
+
+// ""     =>  NULL
+// "1"    =>  NULL
+// "1,2"  =>  NULL
+function array (str) {
+  const nums = str.split(',');
+  str = nums.slice(1, nums.length -1).join(' ');
+  if(str === ''){
+    return null;
+  } else {
+    return str;
+  }
 }
