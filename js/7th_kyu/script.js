bcc28d1cc2851416c3d850bee6a22baff3455600
Kalliran <theautumn90@gmail.com>
2024-04-10 09:41:31 -0500

    completed 7th kyu kata

--- js/7th_kyu/script.js
+++ js/7th_kyu/script.js
@@ -608,3 +608,13 @@ function divisors (integer) {
         return listDivisors;
     }
 };
+//-------------------------------------------------------------------------------------------
+// Complete the function/method so that it returns the url with anything after the anchor (#) removed.
+
+// Examples
+// "www.codewars.com#about" --> "www.codewars.com"
+// "www.codewars.com?page=1" -->"www.codewars.com?page=1"
+function removeUrlAnchor(url){
+    result = url.split('#')[0];
+    return (result);
+}
