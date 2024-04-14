5928eecc30dffb2a50bc701fc52a2b7e8f1ff1b1
Kalliran <theautumn90@gmail.com>
2024-04-14 09:14:30 -0500

    completed 8th kyu kata

--- js/8th_kyu/script.js
+++ js/8th_kyu/script.js
@@ -1043,3 +1043,12 @@ function firstNonConsecutive(arr) {
   }
   return null; // got an undefined error, so had to use this statement
 }
+//-------------------------------------------------------------------
+// The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+
+// Example output:
+
+// Hello, Mr. Spock
+function sayHello(name) {
+  return `Hello, ${name}`;
+}
