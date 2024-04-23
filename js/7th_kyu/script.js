c6ffa93501625fca8496089207bfe09081988798
Kalliran <theautumn90@gmail.com>
2024-04-23 12:28:20 -0500

    completed 7th kyu kata

--- js/7th_kyu/script.js
+++ js/7th_kyu/script.js
@@ -689,3 +689,17 @@ var capitals = function (word) {
     }
     return z;
 };
+//-------------------------------------------------------------------------------------------
+// You will be given an array and a limit value. You must check that all values in the array are below or equal to the limit value. If they are, return true. Else, return false.
+
+// You can assume all values in the array are numbers.
+function smallEnough(a, limit){
+    for (let i = 0; i < a.length; i++){
+        if (a[i] > limit) {
+            return false;
+        }
+    }
+    return true;
+}
+// alternate way
+const smallEnough = (a, limit) => a.every(element => element <= limit);
