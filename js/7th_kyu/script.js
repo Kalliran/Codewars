350996a300c74685b227722b95d07e751eda612e
Kalliran <theautumn90@gmail.com>
2024-04-26 14:24:15 -0500

    completed 7th kyu kata

--- js/7th_kyu/script.js
+++ js/7th_kyu/script.js
@@ -723,3 +723,23 @@ const solutions = nums => nums ? nums.sort((a, b) => a - b) : [];
 // [17, 17, 3, 17, 17, 17, 17] ==> 3
 const stray = (numbers, sort = numbers.sort()) => 
     sort[0] == sort[1] ? sort[sort.length - 1] : sort[0]; // read as "IF sort at 0 index is equal to sort at 1 index, RETURN sort[sort.length -1] ELSE return sort 0 index."
+//-------------------------------------------------------------------------------------------
+// Story
+// Your online store likes to give out coupons for special occasions. Some customers try to cheat the system by entering invalid codes or using expired coupons.
+
+// Task
+// Your mission:
+// Write a function called checkCoupon which verifies that a coupon code is valid and not expired.
+
+// A coupon is no more valid on the day AFTER the expiration date. All dates will be passed as strings in this format: "MONTH DATE, YEAR".
+
+// Examples:
+// checkCoupon("123", "123", "July 9, 2015", "July 9, 2015")  ===  true
+// checkCoupon("123", "123", "July 9, 2015", "July 2, 2015")  ===  false
+function checkCoupon (enteredCode, correctCode, currentDate, expirationDate){
+    return enteredCode === correctCode && Date.parse(currentDate) <= Date.parse(expirationDate);
+};
+// another way
+function checkCoupon (enteredCode, correctCode, currentDate, expirationDate){
+    return enteredCode === correctCode && new Date(currentDate) <= new Date(expirationDate);
+};
