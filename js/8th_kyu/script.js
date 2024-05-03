8094a4890da32e5abd2434213422da94b1ee6ca8
Kalliran <theautumn90@gmail.com>
2024-05-03 09:59:49 -0500

    completed 8th kyu kata

--- js/8th_kyu/script.js
+++ js/8th_kyu/script.js
@@ -1199,3 +1199,24 @@ function findDifference(a, b) {
 function getRealFloor(n) {
   return n > 13 ? n - 2 : n > 0 ? n - 1 : n;
 }
+//-------------------------------------------------------------------
+// Title: Sum The Strings
+// Create a function that takes 2 integers in form of a string as an input, and outputs the sum (also as a string):
+
+// Example: (Input1, Input2 -->Output)
+
+// "4",  "5" --> "9"
+// "34", "5" --> "39"
+// "", "" --> "0"
+// "2", "" --> "2"
+// "-5", "3" --> "-2"
+// Notes:
+
+// If either input is an empty string, consider it as zero.
+
+// Inputs and the expected output will never exceed the signed 32-bit integer limit (2^31 - 1)
+function sumStr(a,b) {
+  if (a === '') a = '0';
+  if (b === '') b = '0';
+  return `${parseInt(a) + parseInt(b)}`;
+}
