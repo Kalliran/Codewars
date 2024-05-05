f23f34f6c18539d19bcb6ef8910bdd1650850e38
Kalliran <theautumn90@gmail.com>
2024-05-05 11:28:03 -0500

    completed 8th kyu kata

--- js/8th_kyu/script.js
+++ js/8th_kyu/script.js
@@ -1252,3 +1252,16 @@ function helloWorld(){
   let str = 'Hello World!';
   console.log(str);
 }
+//-------------------------------------------------------------------
+// Title: get character from ASCII value
+// Write a function which takes a number and returns the corresponding ASCII char for that value.
+
+// Example:
+
+// 65 --> 'A'
+// 97 --> 'a'
+// 48 --> '0
+// For ASCII table, you can refer to http://www.asciitable.com/
+function getChar(c){
+  return String.fromCharCode(c);
+}
