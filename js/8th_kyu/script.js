6e2a0189e2762ea4bfbd4aec085f9aaf0ac843b5
Kalliran <theautumn90@gmail.com>
2024-05-10 08:56:20 -0500

    completed 8th kyu kata

--- js/8th_kyu/script.js
+++ js/8th_kyu/script.js
@@ -1340,3 +1340,24 @@ const expressionMatter = (a, b, c) =>
 // You can use if..else or ternary operator to complete it.
 const saleHotdogs = n => 
   n * (n < 5 ? 100 : n < 10 ? 95 : 90)
+//-------------------------------------------------------------------
+// Title: Lario and Muigi Pipe Problem
+// Issue
+// Looks like some hoodlum plumber and his brother has been running around and damaging your stages again.
+
+// The pipes connecting your level's stages together need to be fixed before you receive any more complaints.
+
+// The pipes are correct when each pipe after the first is 1 more than the previous one.
+
+// Task
+// Given a list of unique numbers sorted in ascending order, return a new list so that the values increment by 1 for each index from the minimum value up to the maximum value (both included).
+
+// Example
+// Input:  1,3,5,6,7,8 Output: 1,2,3,4,5,6,7,8
+function pipeFix(numbers){
+  const newNumbers = [];
+  for (let i = numbers[0]; i <= numbers[numbers.length - 1]; i++){
+    newNumbers.push(i);
+  }
+  return newNumbers;
+}
