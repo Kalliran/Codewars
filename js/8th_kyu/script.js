e2373b5fde5149c149cebef206fb86de49decbac
Kalliran <theautumn90@gmail.com>
2024-05-11 10:07:07 -0500

    completed 8th kyu kata

--- js/8th_kyu/script.js
+++ js/8th_kyu/script.js
@@ -1361,3 +1361,27 @@ function pipeFix(numbers){
   }
   return newNumbers;
 }
+//-------------------------------------------------------------------
+// Title: Draw stairs
+// Given a number n, draw stairs using the letter "I", n tall and n wide, with the tallest in the top left.
+
+// For example n = 3 result in:
+
+// "I\n I\n  I"
+// or printed:
+
+// I
+//  I
+//   I
+// Another example, a 7-step stairs should be drawn like this:
+
+// I
+//  I
+//   I
+//    I
+//     I
+//      I
+//       I
+function drawStairs(n) {
+  return [...Array(n)].map((_, index) => ' '.repeat(index) + 'I').join('\n')
+}
