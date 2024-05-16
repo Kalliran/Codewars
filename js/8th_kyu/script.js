2b2a3569585a2308f4b3dba1888c7831cf62fb19
Kalliran <theautumn90@gmail.com>
2024-05-16 10:07:00 -0500

    completed 8th kyu kata

--- js/8th_kyu/script.js
+++ js/8th_kyu/script.js
@@ -1,18 +1,20 @@
 // Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
 // [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
 
-const grow=x=> x.reduce((a,b) => a*b);
+const grow = x => x.reduce((a,b) => a*b);
 
 // function grow(x){
 //     return x.reduce((acc, c) => acc*c);
 //   }
 //------------------------------------------------------------------
-//You will be given an array a and a value x. All you need to do is check whether the provided array contains the value. Array can contain numbers or strings. X can be either.Return true if the array contains the value, false if not.
+//You will be given an array 'a'and a value 'x'. All you need to do is check whether the provided array contains the value. Array can contain numbers or strings. X can be either.Return true if the array contains the value, false if not.
 
 function check(a, x) {
     // your code here
     return a.includes(x)
 }
+
+const check = (a, x) => a.includes(x);
 //------------------------------------------------------------------
 //You get an array of numbers, return the sum of all of the positives ones.
 
