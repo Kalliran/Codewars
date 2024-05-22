738681920818be9b26330c1d26944fdc086edb3e
Kalliran <theautumn90@gmail.com>
2024-05-22 10:53:58 -0500

    completed 7th kyu kata

--- js/7th_kyu/script.js
+++ js/7th_kyu/script.js
@@ -889,3 +889,8 @@ const sumDigits = (number) =>
       return parseInt(a) + parseInt(b);
     }, 0);
 //-------------------------------------------------------------------------------------------
+// Title: Is it a letter?
+// Write a function, isItLetter or is_it_letter or IsItLetter, which tells us if a given character is a letter or not.
+function isItLetter(character) {
+  return /[a-z]/i.test(character);
+}
