647651e5af6f1c12490090124fc8b3432b941f42
Kalliran <theautumn90@gmail.com>
2024-05-25 08:56:36 -0500

    completed 8th kyu kata

--- python/8th kyu/main.py
+++ python/8th kyu/main.py
@@ -20,3 +20,27 @@ Given [34, -345, -1, 100] your solution will return -345
 You can assume, for the purpose of this kata, that the supplied array will not be empty."""
 def find_smallest_int(arr):
   return min(arr)
+#-----------------------------------------------------------------------------------------
+# Title: CSV representation of array
+'''Create a function that returns the CSV representation of a two-dimensional numeric array.
+
+Example:
+
+input:
+   [[ 0, 1, 2, 3, 4 ],
+    [ 10,11,12,13,14 ],
+    [ 20,21,22,23,24 ],
+    [ 30,31,32,33,34 ]] 
+    
+output:
+     '0,1,2,3,4\n'
+    +'10,11,12,13,14\n'
+    +'20,21,22,23,24\n'
+    +'30,31,32,33,34'
+Array's length > 2.
+
+More details here: https://en.wikipedia.org/wiki/Comma-separated_values
+
+Note: you shouldn't escape the \n, it should work as a new line.'''
+def to_csv_text(array):
+  return "\n".join([",".join(map(str, i)) for i in array])
