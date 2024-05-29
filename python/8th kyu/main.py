a8f7c80c483e900690c156f47e00dd138251127c
kalliran <theautumn90@gmail.com>
2024-05-29 09:36:27 -0500

    completed 8th kyu kata

--- python/8th kyu/main.py
+++ python/8th kyu/main.py
@@ -56,3 +56,11 @@ def multiply(a, b):
 Note: Empty arrays should return 0.'''
 def find_average(numbers):
   return sum(numbers) / len(numbers) if numbers else 0
+#-----------------------------------------------------------------------------------------
+# Title: Sentence Smash
+'''Write a function that takes an array of words and smashes them together into a sentence and returns the sentence. You can ignore any need to sanitize words or add punctuation, but you should add spaces between each word. Be careful, there shouldn't be a space at the beginning or the end of the sentence!
+
+Example
+['hello', 'world', 'this', 'is', 'great']  =>  'hello world this is great'''
+def smash(words):
+  return " ".join(words)
