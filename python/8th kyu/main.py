f7dae80ab555d0b984e733cbc2997ff6480009f6
Kalliran <theautumn90@gmail.com>
2024-06-07 11:36:14 -0500

    completed 8th kyu kata

--- python/8th kyu/main.py
+++ python/8th kyu/main.py
@@ -82,3 +82,15 @@ If either input is an empty string, consider it as zero.
 Inputs and the expected output will never exceed the signed 32-bit integer limit (2^31 - 1)"""
 def sum_str(a, b):
   return str(int(a or 0) + int(b or 0))
+#-----------------------------------------------------------------------------------------
+# Title: Transportation on Vacation
+'''After a hard quarter in the office you decide to get some rest on a vacation. So you will book a flight for you and your girlfriend and try to leave all the mess behind you.
+
+You will need a rental car in order for you to get around in your vacation. The manager of the car rental makes you some good offers.
+
+Every day you rent the car costs $40. If you rent the car for 7 or more days, you get $50 off your total. Alternatively, if you rent the car for 3 or more days, you get $20 off your total.
+
+Write a code that gives out the total amount for different days(d).
+'''
+def rental_car_cost(d):
+  return d * 40 - (50 if d >= 7 else 20 if d >= 3 else 0)
