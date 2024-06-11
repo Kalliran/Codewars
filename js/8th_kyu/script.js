c64d88e14f794083201626c0d0d19be23e646bea
Kalliran <theautumn90@gmail.com>
2024-06-10 20:54:42 -0500

    completed 8th kyu kata

--- js/8th_kyu/script.js
+++ js/8th_kyu/script.js
@@ -1544,3 +1544,12 @@ function problem(x) {
 function mouthSize (animal) {
   return animal.toLowerCase() === 'alligator' ? 'small' : 'wide';
 }
+//-------------------------------------------------------------------
+// Title: Sort My Textbooks
+// HELP! Jason can't find his textbook! It is two days before the test date, and Jason's textbooks are all out of order! Help him sort a list (ArrayList in java) full of textbooks by subject, so he can study before the test.
+
+// The sorting should NOT be case sensitive
+function sorter (textbooks) {
+  return textbooks.sort((a, b) => a.toLowerCase() > b.toLowerCase() ? 1 : 
+                                  a.toLowerCase() < b.toLowerCase() ? -1  : 0)
+}
